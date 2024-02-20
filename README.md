dee2cbb82d7baa9c49c54954feac681d25fae0ac
Jedidiah Solomon Onwubiko <121108148+Jedidiah-Solomon@users.noreply.github.com>
2024-02-20 23:22:34 +0100

    Updated Js File

--- README.md
+++ README.md
@@ -9,7 +9,11 @@
 - Developed first with the Mobile First methodology, then for desktop.
 - Compatible with all mobile devices and with a beautiful and pleasant user interface.
 
-💙 View the site at. [Hungsmeal Group](https://jedidiah-solomon.github.io/Hungsmeal-Resturant-Website--Solomon/))
+### External Sites
+1. https://boxicons.com/ for box icons
+2. https://scrollrevealjs.org/
+
+💙 View the site at. [Hungsmeal Group](https://jedidiah-solomon.github.io/Hungsmeal-Resturant-Website--Solomon/)
 
 Contact Details: 
 > ***+2347062957194*** 
--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,11 @@ window.addEventListener('scroll', scrollActive)
 function scrollHeader(){
     const nav = document.getElementById('header')
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if (this.scrollY >= 200) {
+        nav.classList.add('scroll-header');
+    } else {
+        nav.classList.remove('scroll-header');
+    }    
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -62,39 +66,70 @@ window.addEventListener('scroll', scrollHeader)
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    if(this.scrollY >= 560) {
+         scrollTop.classList.add('show-scroll');
+     } else {
+         scrollTop.classList.remove('show-scroll');
+     }
 }
-window.addEventListener('scroll', scrollTop)
+window.addEventListener('scroll', scrollTop);
 
 /*==================== DARK LIGHT THEME ====================*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+//---------------------------------------------------------------------------//
+// Function to get the current theme
+function getCurrentTheme() {
+    if (document.body.classList.contains(darkTheme)) {
+        return 'dark';
+    } else {
+        return 'light';
+    }
 }
 
+// Function to get the current icon
+function getCurrentIcon() {
+    if (themeButton.classList.contains(iconTheme)) {
+        return 'bx-moon';
+    } else {
+        return 'bx-sun';
+    }
+}
+//---------------------------------------------------------------------------//
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    if (selectedTheme === 'dark') {
+        document.body.classList.add(darkTheme);
+    } else {
+        document.body.classList.remove(darkTheme);
+    }
+
+    if (selectedIcon === 'bx-moon') {
+        themeButton.classList.add(iconTheme);
+    } else {
+        themeButton.classList.remove(iconTheme);
+    }
+}
+
+
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener('click', function() {
     // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    
+    // We save the theme and the current icon that the user choose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
@@ -112,3 +147,6 @@ sr.reveal(`.home__data, .home__img,
             .footer__content`, {
     interval: 200
 })
+
+// Scroll reveal individually using the h2 .headline class in About Section
+ScrollReveal().reveal('.headline');
